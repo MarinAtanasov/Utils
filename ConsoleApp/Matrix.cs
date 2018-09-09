ae76ccfc6030358237571771c4627d99681f0943
MarinAtanasov <marin.atanasov.bg@gmail.com>
2018-09-09 11:03:47 +0300

    Add Matrix

---
+++ ConsoleApp/Matrix.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    public class Matrix
+    {
+        #region Construction
+        public Matrix(int cols, params double[] elements) :
+            this(cols > 0 ? (elements?.Length ?? 0) / cols : 0, cols, elements)
+        {
+        }
+
+        public Matrix(IReadOnlyCollection<IReadOnlyCollection<double>> elements) :
+            this(elements?.Count ?? 0, elements?.FirstOrDefault()?.Count ?? 0, elements?.SelectMany(x => x ?? Array.Empty<double>()))
+        {
+        }
+
+        public Matrix(int rows, int cols, IEnumerable<double> elements)
+        {
+            if (rows < 0)
+                throw new ArgumentOutOfRangeException($"{nameof(rows)} = {rows}");
+            if (cols < 0)
+                throw new ArgumentOutOfRangeException($"{nameof(cols)} = {cols}");
+            if (elements == null)
+                throw new ArgumentNullException(nameof(elements));
+
+            this.Rows = rows;
+            this.Columns = cols;
+            this.elements = new double[rows][];
+            using (var enumerator = elements.GetEnumerator())
+            {
+                for (int r = 0; r < rows; r++)
+                {
+                    var row = new double[cols];
+                    for (int c = 0; c < cols; c++)
+                    {
+                        enumerator.MoveNext();
+                        row[c] = enumerator.Current;
+                    }
+                    this.elements[r] = row;
+                }
+            }
+        }
+
+        private Matrix(int rows, int cols, double[][] elements)
+        {
+            this.Rows = rows;
+            this.Columns = cols;
+            this.elements = elements;
+        }
+        #endregion
+
+        #region Properties
+        public int Rows { get; }
+
+        public int Columns { get; }
+
+        public IReadOnlyCollection<double> this[int row] => this.elements[row];
+
+        public double this[int row, int column] => this.elements[row][column];
+        #endregion
+
+        #region Public methods
+        public static Matrix operator +(Matrix left, Matrix right)
+        {
+            if (left == null)
+                throw new ArgumentNullException(nameof(left));
+            if (right == null)
+                throw new ArgumentNullException(nameof(right));
+            if (left.Rows != right.Rows || left.Columns != right.Columns)
+                throw new ArgumentException($"Invalid matrix addition: {left.Rows}x{left.Columns} + {right.Rows}x{right.Columns}");
+
+            var rows = left.Rows;
+            var cols = right.Columns;
+            var leftElements = left.elements;
+            var rightElements = right.elements;
+
+            var result = new double[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                var leftRow = leftElements[row];
+                var rightRow = rightElements[row];
+                var resultRow = new double[cols];
+                for (int col = 0; col < cols; col++)
+                {
+                    resultRow[col] = leftRow[col] + rightRow[col];
+                }
+                result[row] = resultRow;
+            }
+
+            return new Matrix(rows, cols, result);
+        }
+
+        public static Matrix operator +(Matrix left, double right)
+        {
+            if (left == null)
+                throw new ArgumentNullException(nameof(left));
+
+            var rows = left.Rows;
+            var cols = left.Columns;
+            var leftElements = left.elements;
+
+            var result = new double[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                var leftRow = leftElements[row];
+                var resultRow = new double[cols];
+                for (int col = 0; col < cols; col++)
+                {
+                    resultRow[col] = leftRow[col] + right;
+                }
+                result[row] = resultRow;
+            }
+
+            return new Matrix(rows, cols, result);
+        }
+
+        public static Matrix operator +(double left, Matrix right) => right + left;
+
+        public static Matrix operator -(Matrix left, Matrix right)
+        {
+            if (left == null)
+                throw new ArgumentNullException(nameof(left));
+            if (right == null)
+                throw new ArgumentNullException(nameof(right));
+            if (left.Rows != right.Rows || left.Columns != right.Columns)
+                throw new ArgumentException($"Invalid matrix subtraction: {left.Rows}x{left.Columns} - {right.Rows}x{right.Columns}");
+
+            var rows = left.Rows;
+            var cols = right.Columns;
+            var leftElements = left.elements;
+            var rightElements = right.elements;
+
+            var result = new double[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                var leftRow = leftElements[row];
+                var rightRow = rightElements[row];
+                var resultRow = new double[cols];
+                for (int col = 0; col < cols; col++)
+                {
+                    resultRow[col] = leftRow[col] - rightRow[col];
+                }
+                result[row] = resultRow;
+            }
+
+            return new Matrix(rows, cols, result);
+        }
+
+        public static Matrix operator -(Matrix left, double right) => left + (right * -1);
+
+        public static Matrix operator -(double left, Matrix right) => right - left;
+
+        public static Matrix operator *(Matrix left, Matrix right)
+        {
+            if (left == null)
+                throw new ArgumentNullException(nameof(left));
+            if (right == null)
+                throw new ArgumentNullException(nameof(right));
+            if (left.Columns != right.Rows)
+                throw new ArgumentException($"Invalid matrix multiplication: {left.Rows}x{left.Columns} * {right.Rows}x{right.Columns}");
+
+            var rows = left.Rows;
+            var cols = right.Columns;
+            var common = left.Columns;
+            var leftElements = left.elements;
+            var rightElements = right.Transpose().elements;
+
+            var result = new double[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                var leftRow = leftElements[row];
+                var resultRow = new double[cols];
+                for (int col = 0; col < cols; col++)
+                {
+                    var rightRow = rightElements[col];
+                    var resultRowCol = 0.0;
+                    for (int x = 0; x < common; x++)
+                    {
+                        resultRowCol += leftRow[x] * rightRow[x];
+                    }
+                    resultRow[col] = resultRowCol;
+                }
+                result[row] = resultRow;
+            }
+
+            return new Matrix(rows, cols, result);
+        }
+
+        public static Matrix operator *(Matrix left, double right)
+        {
+            if (left == null)
+                throw new ArgumentNullException(nameof(left));
+
+            var rows = left.Rows;
+            var cols = left.Columns;
+            var leftElements = left.elements;
+
+            var result = new double[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                var leftRow = leftElements[row];
+                var resultRow = new double[cols];
+                for (int col = 0; col < cols; col++)
+                {
+                    resultRow[col] = leftRow[col] * right;
+                }
+                result[row] = resultRow;
+            }
+
+            return new Matrix(rows, cols, result);
+        }
+
+        public static Matrix operator *(double left, Matrix right) => right * left;
+
+        public static Matrix operator /(Matrix left, double right) => left * (1 / right);
+
+        public static Matrix operator /(double left, Matrix right)
+        {
+            if (right == null)
+                throw new ArgumentNullException(nameof(right));
+
+            var rows = right.Rows;
+            var cols = right.Columns;
+            var rightElements = right.elements;
+
+            var result = new double[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                var rightRow = rightElements[row];
+                var resultRow = new double[cols];
+                for (int col = 0; col < cols; col++)
+                {
+                    resultRow[col] = left / rightRow[col];
+                }
+                result[row] = resultRow;
+            }
+
+            return new Matrix(rows, cols, result);
+        }
+
+        public Matrix Transpose()
+        {
+            var elements = new double[this.Columns][];
+            for (int c = 0; c < this.Columns; c++)
+            {
+                var row = new double[this.Rows];
+                for (int r = 0; r < this.Rows; r++)
+                {
+                    row[r] = this.elements[r][c];
+                }
+                elements[c] = row;
+            }
+            return new Matrix(this.Columns, this.Rows, elements);
+        }
+        #endregion
+
+        #region Private fields and constants
+        private readonly double[][] elements;
+        #endregion
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var stopwatch = Stopwatch.StartNew();
+
+            var a = new double[512][];
+            Enumerable.Range(0, a.Length).ToList().ForEach(row =>
+            {
+                a[row] = Enumerable.Range(0, 512).Select(col => (250.0 - col) / 10).ToArray();
+            });
+            var b = new double[a[0].Length][];
+            Enumerable.Range(0, b.Length).ToList().ForEach(row =>
+            {
+                b[row] = Enumerable.Range(0, 512).Select(col => (250.0 - col) / -10).ToArray();
+            });
+
+            var left = new Matrix(a);
+            var right = new Matrix(b);
+            stopwatch.Restart();
+            var result = left * right;
+            Console.WriteLine($"Multiplied in {stopwatch.Elapsed.TotalMilliseconds} ms.");
+
+            stopwatch.Restart();
+            Enumerable.Range(0, 10).ToList().ForEach(x => result = left * right);
+
+            Console.WriteLine("Executed in: {0} ms.", stopwatch.Elapsed.TotalMilliseconds);
+        }
+    }
+}
